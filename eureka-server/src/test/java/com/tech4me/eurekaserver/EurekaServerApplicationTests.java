8cae66b8e827e8afa28d201d111b2b6b6e821caa
Weberson.dsn.erp <weberson.dsn.erp@alterdata.com.br>
2021-09-27 21:54:11 -0300

    Subindo versão inicial do nosso projeto de microserviços com Java.

---
+++ eureka-server/src/test/java/com/tech4me/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech4me.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
